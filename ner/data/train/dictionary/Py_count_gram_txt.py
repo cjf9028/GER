b906e19ab35b384be7509c44db863b7a8a163dbf
snaag <04_miffy@naver.com>
2018-12-11 18:35:58 +0900

    [dict] 사전 및 카운팅, 각각 스크립트 파일 만듦 만들어진 사전: gram2.txt, gram3.txt, gram4.txt 사전 카운팅: gram2_count.txt, gram3_count.txt, gram4_count.txt 사전 만드는 스크립트: Py_dict_make_in.py, Py_dict_make_out.py 사전 카운팅 만드는 스크립트: Py_count_gram_txt.py

--- ner/data/train/dictionary/Py_count_gram_txt.py
+++ ner/data/train/dictionary/Py_count_gram_txt.py
@@ -1,1 +1,39 @@
-fin
+import io
+
+nameInList  = ["gram2.txt", "gram3.txt", "gram4.txt"]
+nameOutList = list()
+cntIn       = 3 # how many input file
+fInList     = list()
+fOutList    = list()
+dictWord    = dict()
+
+#make Out file (use input file name)
+for idx in range(cntIn):
+    s = nameInList[idx].split(".")[0]+"_count.txt"
+    nameOutList.append(s)
+    fOutList.append(io.open(nameOutList[idx], encoding='utf-8', mode='w'))
+
+# fileOpen
+for idx in range(cntIn):
+    fInList.append (io.open(nameInList[idx], encoding='utf-8', mode='r+'))
+
+# read each file
+for each in range(cntIn):
+    dictWord = dict() # re-initialize at each file (gram2.txt, gram3.txt ...)
+    while True:
+        line = fInList[each].readline()
+        if not line: break
+        line = line.replace("\n", "")
+
+        if len(line) > 1 :
+#            print(line)
+            if line in dictWord.keys():
+                dictWord[line] = dictWord[line]+1
+            else:
+                dictWord[line] = 1
+
+# Write on each out file
+    for key, value in dictWord.items():
+#        print(key+", "+str(value))
+        fOutList[each].write(key+"\t"+str(value)+"\n")
+
--- ner/data/train/dictionary/Py_dict_make_in.py
+++ ner/data/train/dictionary/Py_dict_make_in.py
@@ -13,7 +13,7 @@ nameList    = ["_", "_", "gram2.txt", "gram3.txt", "gram4.txt"]
 fOutList    = list()
 dictWord    = dict()
 
-# fileOpen
+# open file
 for idx in range(0, idxStart):
     print("idx is "+str(idx))
     fOutList.append("_")
@@ -25,7 +25,7 @@ while True:
     line = fin.readline()
     if not line: break
     # idxStart ~ idxEnd for
-    # Split with each count (2, 3, 4)
+    # Split with each count (2, 3, 4 ~ same with gram)
     
     try:
         tag = line.split("\t")[2].replace("\n","")
@@ -36,15 +36,15 @@ while True:
             newTag = tag.split("_")[0]
             print(word)
             
-            for gram in range(idxStart+1, idxEnd+2): # change to idxEnd+1
+            for gram in range(idxStart+1, idxEnd+2):
                 ll = len(word)
-                s = "_"+word[0:gram-1] # count dict
-                #
+                s = "_"+word[0:gram-1]]
+                
                 fOutList[gram].write(s+unicode("\t"+newTag+"\n"))
                 for idx in range(ll):
                     
                     if idx+gram <= ll :
-                        fOutList[gram].write(word[idx:idx+gram]) # count dict #
+                        fOutList[gram].write(word[idx:idx+gram])
                     else:
                         fOutList[gram].write(unicode("_\t"+newTag+"\n"))
                         
@@ -61,20 +61,3 @@ fin.close()
 
 for idx in range(idxStart, idxEnd+1):
     fOutList[idx].close()
-
-
-#def addWordToDict(val, type) :
-#    #    val:   넘길 text
-#    #    type:  1=시작&진행중, -1=종료
-#
-#    if type is 1:
-#        if val in dictWord.keys():
-#            dictWord[word] = dictWord[word] + 1
-#        else:
-#            dictWord[word] = 0
-#
-#    if type is -1:
-#        if val+"_"
-
-
-
