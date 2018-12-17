491c5e06b997a48f282165a33f00ad66f236a5fc
cjf9028 <18022635+cjf9028@users.noreply.github.com>
2018-12-17 16:16:55 +0900

    Add files via upload
    음절 추가

--- ner/data_loader.py
+++ ner/data_loader.py
@@ -1,24 +1,39 @@
 # -*- coding: utf-8 -*-
 
 import os
-
+from konlpy.tag import Komoran
 def _read_data_file(file_path, train=True):
     sentences = []
-    sentence = [[], [], []]
+    sentence = [[], [], [],[]]
+    komoran = Komoran()
     for line in open(file_path, encoding="utf-8"):
         line = line.strip()
-        if line == "":
-            sentences.append(sentence)
-            sentence = [[], [], []]
-        else:
-            idx, ejeol, ner_tag = line.split("\t")
-            # idx는 0부터 시작하도록 
-            sentence[0].append(int(idx))
-            sentence[1].append(ejeol)
-            if train:
-                sentence[2].append(ner_tag)
-            else:
-                sentence[2].append("-")
+		if max < index:
+			max = index
+		if line == "":
+			sentences.append(sentence)
+			sentence = [[], [], []]
+		else:
+			idx, ejeol, ner_tag = line.split("\t")
+			# idx는 0부터 시작하도록
+			if int(idx) == 1:
+				index = 1
+			for x in range(0, len(ejeol)):
+				sentence[0].append(int(index))
+				index += 1
+				sentence[1].append(ejeol[x])
+				if x == 0 and ner_tag != "-":
+					if ner_tag[-1:] == "I":
+						ner_tag = ner_tag[:-1] + "l"
+					else:
+						ner_tag = ner_tag[:-1] + "B"
+				elif ner_tag != "-":
+					ner_tag = ner_tag[:-1] + "l"
+				sentence[2].append(ner_tag)
+			sentence[0].append(int(index))
+			index += 1
+			sentence[1].append("SP_B")
+			sentence[2].append("SP_B")
 
     return sentences
 
